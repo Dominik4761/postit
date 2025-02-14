5e3dd45cc592d4e8d2d5cdf5aa0a6897dbd2b289
Dominik4761 <doczi.dominik.adam@gmail.com>
2025-02-14 10:03:52 +0100

    Add project files.

---
+++ postit/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace postit
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Post v1 = new Post("sarga", " ");
+			Post v2 = new Post("zold", " ");
+			Post v3 = new Post("Kek", " ");
+
+			v1.Tartalom = "zab, tej, liszt";
+			v2.Tartalom = "+3670888459";
+			v3.Tartalom = "16:00 Fogászat";
+
+
+			Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Szín: {v1.Szin} Tartalom: {v1.Tartalom}");
+			Console.BackgroundColor = ConsoleColor.Green;
+			Console.WriteLine($"Szín: {v2.Szin} Tartalom: {v2.Tartalom}");
+			Console.BackgroundColor = ConsoleColor.Blue;
+			Console.WriteLine($"Szín: {v3.Szin} Tartalom: {v3.Tartalom}");
+			Console.BackgroundColor = ConsoleColor.Black;
+		}
+	}
+}
---
+++ postit/post.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace postit
+{
+	internal class Post
+	{
+		private string szin;
+		private string tartalom;
+
+		public Post(string szin, string tartalom)
+		{
+			this.szin = szin;
+			this.tartalom = tartalom;
+		}
+
+		public string Szin
+		{
+			get
+			{ 
+				return szin;
+			}
+		}
+		public string Tartalom
+		{
+			get
+			{
+				return tartalom;
+			}
+			set
+			{
+				tartalom = value;
+			}
+		}
+
+		public string Szin1 { get => szin; }
+		public string Tartalom1 { get => tartalom; set => tartalom = value; }
+
+		public override string ToString()
+		{
+			return $"{this.Szin} {this.tartalom}";
+		}
+	}
+}
